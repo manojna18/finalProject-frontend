41d3f2be809e9a73b80855958cd670e3a8a13710
manojna18 <kalyani.manojna@gmail.com>
2024-07-05 13:07:16 -0400

    recipe page

--- final-project/src/components/Header.tsx
+++ final-project/src/components/Header.tsx
@@ -1,21 +1,33 @@
-import { Link } from 'react-router-dom';
-import './css/Header.css'
+import { Link } from "react-router-dom";
+import "./css/Header.css";
 
 const Header = () => {
   return (
-    <header className='Header'>
-        <h1><Link to="/">PlantPlate</Link></h1>
-        <nav>
-            <ul>
-                <li><Link to="/favorites">Favorites</Link></li>
-                <li><Link to="/tracker">Nutrition Tracker</Link></li>
-                <li><Link to="/barcode-scanner">Barcode Scanner</Link></li>
-                <li><Link to="/goals">Goals</Link></li>
-
-            </ul>
-        </nav>
+    <header className="Header">
+      <h1>
+        <Link to="/">PlantPlate</Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/favorites">Favorites</Link>
+          </li>
+          <li>
+            <Link to="/tracker">Nutrition Tracker</Link>
+          </li>
+          <li>
+            <Link to="/barcode-scanner">Barcode Scanner</Link>
+          </li>
+          <li>
+            <Link to="/recipes/:id">Recipes</Link>
+          </li>
+          <li>
+            <Link to="/goals">Goals</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 };
 
 export default Header;
--- final-project/src/components/Recipe.tsx
+++ final-project/src/components/Recipe.tsx
@@ -1,11 +1,57 @@
-import './css/Recipe.css'
+import { FormEvent, useEffect, useState } from "react";
+import "./css/Recipe.css";
+import { getRecipe } from "../services/edamamApiService";
+import RecipeInterface from "../models/Recipe";
 
 const Recipe = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [veganOnly, setVeganOnly] = useState(true);
+  const [recipeList, setRecipeList] = useState<RecipeInterface[]>([]);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    getRecipe(searchTerm).then((res) => {
+      setRecipeList(res.hits.map((item) => item.recipe));
+    });
+    setSearchTerm("");
+  };
+
   return (
-    <div className='Recipe'>
-        Recipe works
+    <div className="Recipe">
+      <form onSubmit={(e) => handleSubmit(e)} id="search-form">
+        <h3>Search for recipes with a keyword(eg: "avocado", "salad")</h3>
+        <label htmlFor="search-bar">Enter a keyword</label>
+        <input
+          type="text"
+          value={searchTerm}
+          id="search-bar"
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <label htmlFor="veg">Also show vegetarian recipes</label>
+        <input
+          type="checkbox"
+          id="veg"
+          onClick={() => setVeganOnly(!veganOnly)}
+        />
+        <button type="submit">Get Recipes</button>
+      </form>
+      <div className="recipes-container">
+        {recipeList.map((recipe) => (
+          <>
+            <h3>{recipe.label}</h3>
+            {recipe.ingredientLines.map((ing) => (
+              <p>{ing}</p>
+            ))}
+            <p>Meal Type: {recipe.mealType}</p>
+            <img src={recipe.image} />
+            <a href={recipe.url}>Read more</a>
+          </>
+        ))}
+      </div>
     </div>
-  )
+  );
 };
 
 export default Recipe;
